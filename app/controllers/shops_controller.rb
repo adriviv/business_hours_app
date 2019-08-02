d96450221d9ae1cbeb1d40baf43b5ed9f6b1d6cb
Adrien Viviani <adrien.viv@gmail.com>
2019-08-02 12:36:03 +0200

    Shop CRUD : Edit + Update done

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,5 @@
 class ShopsController < ApplicationController
-    # before_action :set_shops, only: [edit]
+    before_action :set_shops, only: [:edit, :update]
 
     def new
         @shop = Shop.new 
@@ -15,11 +15,23 @@ class ShopsController < ApplicationController
             render :new
         end
     end 
+
+    def edit
+    end
+
+    def update
+        if @shop.update(shop_params)
+            redirect_to '/home.html'
+        else 
+            redirect_to root_path
+        end  
+    end
     
- 
-
-
     private
+
+    def set_shops
+        @shop = Shop.find(params[:id])
+    end
 
     def shop_params
         params.require(:shop).permit(:name, business_opening_hours_attributes: [:day, :opening_time, :closing_time, :break_starting_time, :break_finishing_time])
