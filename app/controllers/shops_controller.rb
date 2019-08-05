62fda9cd78e5be1e36604ee4a2b01bd3a5f817e9
Adrien Viviani <adrien.viv@gmail.com>
2019-08-05 22:58:24 +0200

    intermediate commit: Moove Opening_time logic from View to Controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,9 +2,20 @@
 
 class ShopsController < ApplicationController
   before_action :set_shops, only: %i[show edit update]
+  # before_action :order_by_current_day
 
   def index
     @shops = Shop.all
+
+    weekdays = Date::DAYNAMES[0..6]  
+    weekdays_rotate = weekdays.rotate!(weekdays.index(Time.now.strftime("%A")))  
+    weekdays_rotate_integer = weekdays_rotate.map do |lol|  
+      Date.parse(lol).strftime('%w')  
+    end
+  
+    @case_pieces = weekdays_rotate_integer.each_with_index.map do |day, i| 
+      "WHEN '#{day}' THEN #{i}" 
+    end   
   end
 
   def show; end
@@ -42,4 +53,26 @@ class ShopsController < ApplicationController
                                    break_finishing_time
                                  ])
   end
+
+def order_by_current_day
+  daysname = Date::DAYNAMES[0..6]  
+  daysname_rotate = daysname.rotate!(daysname.index(Time.now.strftime("%A")))  
+  
+  integer_name = daysname_rotate.map do |lol|  
+    Date.parse(lol).strftime('%w')  
+  end
+
+  case_pieces = integer_name.each_with_index.map do |day, i| 
+    "WHEN '#{day}' THEN #{i}" 
+  end 
+
+  # @shop.business_opening_hours.all.order("CASE day #{case_pieces.join(' ')} END").each_with_index do |hour, index|
+
+
+end
+
+
+
+
+
 end
